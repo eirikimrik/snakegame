995a2142ebcc707d8523de24c6b0be0a0823342b
Eirik <eirik.imrik@gmail.com>
2024-08-21 20:32:21 +0200

    getting familiar with swing library.

--- src/main/java/org/eirikimrik/Main.java
+++ src/main/java/org/eirikimrik/Main.java
@@ -1,7 +1,24 @@
 package org.eirikimrik;
 
+import javax.swing.*;
+import java.awt.Color;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("My first frame");
+        //JLabel label = new JLabel("Hello world");
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.green);
+
+
+        //label.setHorizontalAlignment(0);
+
+        frame.setSize(300,300);
+        frame.add(panel);
+
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
